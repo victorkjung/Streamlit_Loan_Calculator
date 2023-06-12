abfe57ac38aba85908d70bce9bd2ac41548d3f93
Victor Jung <123326026+victorkjung@users.noreply.github.com>
2023-06-11 23:20:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if calculate:
     monthly_payment, schedule = amortization_schedule(loan_amount, interest_rate, years)
 
     # Display the monthly payment
-    st.markdown(f'Your Monthly Loan Payment is: **{monthly_payment:,.2f}**')
+    st.markdown(f'<h4>; Your Monthly Loan Payment is: **$**{monthly_payment:,.2f};</h4>')
 
     # Format the schedule for display
     schedule["Payment"] = schedule["Payment"].apply(lambda x: '{:,.2f}'.format(x))
